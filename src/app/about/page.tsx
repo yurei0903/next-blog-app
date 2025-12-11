0d69a52fe01aafa74bcb27cc3c8f486ea98843e1
yurei <167486448+yurei0903@users.noreply.github.com>
2025-12-11 14:30:46 +0900

    update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,26 +1,46 @@
 "use client";
-import Image from "next/image";
-import { twMerge } from "tailwind-merge";
+import { useState, useEffect } from "react";
+import type { Post } from "@/app/_types/Post";
+import PostSummary from "@/app/_components/PostSummary";
+import dummyPosts from "@/app/_mocks/dummyPosts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const Page: React.FC = () => {
+  // 投稿データを「状態」として管理 (初期値はnull)
+  const [posts, setPosts] = useState<Post[] | null>(null);
+
+  // コンポーネントが読み込まれたときに「1回だけ」実行する処理
+  useEffect(() => {
+    // 本来はウェブAPIを叩いてデータを取得するが、まずはモックデータを使用
+    // (ネットからのデータ取得をシミュレートして１秒後にデータをセットする)
+    const timer = setTimeout(() => {
+      console.log("ウェブAPIからデータを取得しました (虚言)");
+      setPosts(dummyPosts);
+    }, 1000); // 1000ミリ秒 = 1秒
+
+    // データ取得の途中でページ遷移したときにタイマーを解除する処理
+    return () => clearTimeout(timer);
+  }, []);
+
+  // 投稿データが取得できるまでは「Loading...」を表示
+  if (!posts) {
+    return (
+      <div className="text-gray-500">
+        <FontAwesomeIcon icon={faSpinner} className="mr-1 animate-spin" />
+        Loading...
+      </div>
+    );
+  }
+
+  // 投稿データが取得できたら「投稿記事の一覧」を出力
   return (
     <main>
-      <div className="mb-5 text-2xl font-bold">About</div>
-
-      <div
-        className={twMerge(
-          "mx-auto mb-5 w-full md:w-2/3",
-          "flex justify-center",
-        )}
-      >
-        <Image
-          src="/images/ango.png"
-          alt="Example Image"
-          width={350}
-          height={350}
-          priority
-          className="rounded-full border-4 border-slate-500 p-1.5"
-        />
+      <div className="mb-2 text-2xl font-bold">Main</div>
+      <div className="space-y-3">
+        {posts.map((post) => (
+          <PostSummary key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
